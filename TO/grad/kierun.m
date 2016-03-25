90bd6b86f24eff7c7b961e9cb76006080504972b
radluki <radluki@gmail.com>
2016-03-25 01:44:13 +0100

    Spr Grad

--- TO/grad/kierun.m
+++ TO/grad/kierun.m
@@ -1,8 +1,8 @@
 function [x0,wskaz]=kierun(x0,d)
 
-% KIERUN Przeszukuje kierunek d wychodz¹c z punktu x0.
+% KIERUN Przeszukuje kierunek d wychodzï¿½c z punktu x0.
 
-[zw,qw,wskaz,z,q]=ekskon(x0,d,1,0.1,1e-8);
+[zw,qw,wskaz,z,q]=ekskon(x0,d,1,3,1e-8);
 if wskaz>1
    [zw,qw,z,q]=zlopod(x0,d,zw,qw,5,z,q);
    [zw,qw,z,q]=apropa(x0,d,zw,qw,1,z,q);
--- TO/grad/testy.m
+++ TO/grad/testy.m
@@ -5,13 +5,35 @@ par = 0
 global a
 global zad
 global rodz_grad
-rodz_grad=0;
-zad = 1
+rodz_grad=1;
+zad = 5
 a = 10
 for i=0:3
-    x0=[-2;3];
+    x0=[-3;4];
     par = i;
     granad; 
 end
-% par=3
-% granad
+
+%% Funkcja kwadratowa
+% Między metodami 1,2,3 nie było różnicy w przebiegu. Wzory wyznaczały
+% dokładnie te same kierunki. Jest to zgodne z rozumowaniem przeprowadzonym
+% w książce Metody optymalizacji z ćwiczeniami lab. rozdz. 3.4.2
+% Czas znalezienia rozwiązania najmniejszy był dla metody 1. Najgorszy dla
+% metody 0, która zamiast 2 iteracji przeprowadziła 5 poszukiwań na
+% kierunku dla a=10 x0=[-2,3].
+
+%% Dolina bananowa
+% e0=1e-3
+% x0=[-2,3]
+% maxit=200
+% Gradient analityczny
+% Czas dla metod 1:3 ok. 3 razy krutszy niż dla gradientu numerycznego,
+% liczba iteracji bez zmian.
+% Metoda najszybszego spadku nie znajduje rozwiązania. 
+% Gradient numeryczny - metoda Eulera
+% Czas wykonania: 3) 0.01 2) 0.0125 1) 0.0154 0) 0.1
+% Iteracje: 3) 21 2) 26 1) 34 0) 180
+% Dla metod gradientu sprzężonego z reguły każdy krok daje poprawę 
+% oszacowania wyniku. Odległość od rozwiązania dla metody najszybszego
+% spadku ma charakter skokowy. Niekiedy nawet przez 100 iteracji
+% rozwiązanie może się prawie nic nie poprawić.
